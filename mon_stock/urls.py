2a90a8a5a285925b51fb6f8c863ce1fc7cbf9edb
dienghadji1 <dieng.hadji1@uam.edu.sn>
2026-04-11 12:51:23 +0000

    Ajout_Athentification_admin_vendeur

--- mon_stock/urls.py
+++ mon_stock/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
 router = DefaultRouter()
 router.register(r'categories', views.CategorieViewSet)
 router.register(r'fournisseurs', views.FournisseurViewSet)
